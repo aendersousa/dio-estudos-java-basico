44fbf7e155bd362deb95b028c50a8126e46b08b9
Aender <aenderal3ida@gmail.com>
2024-11-12 20:41:28 -0300

    2

--- tipos-variaveis/src/Tiposvariaveis.java
+++ tipos-variaveis/src/Tiposvariaveis.java
@@ -1,6 +1,6 @@
 public class Tiposvariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("ola");
         
     }
 }
